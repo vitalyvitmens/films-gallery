1f0a9782a8d16c369bfef826751bc568f1fe280e
vitalyvitmens <otm_@tut.by>
2023-05-04 09:27:14 +0300

    first commit

---
+++ filmsMock.js
@@ -0,0 +1,37 @@
+export const filmsMock = [
+  {
+    id: 1,
+    imgUrl: 'assets/fightClub.jpg',
+    title: 'Fight Club',
+    description: 'Very soap film',
+    isFavorite: false,
+  },
+  {
+    id: 2,
+    imgUrl: 'assets/drive.jpg',
+    title: 'Drive',
+    description: `Wow! That's Ryan Gosling in this film`,
+    isFavorite: false,
+  },
+  {
+    id: 3,
+    imgUrl: 'assets/americanPsycho.jpg',
+    title: 'American psycho',
+    description: `Maybe it's only in your mind`,
+    isFavorite: false,
+  },
+  {
+    id: 4,
+    imgUrl: 'assets/limitless.jpg',
+    title: 'Limitless',
+    description: `Where can I find this pill`,
+    isFavorite: false,
+  },
+  {
+    id: 5,
+    imgUrl: 'assets/joker.jpg',
+    title: 'Joker',
+    description: `Tragedy of sad clown :(`,
+    isFavorite: false,
+  },
+]
